bdfd1d801e910a8cc8d0016b71144153e1428559
denglang <denglang@hortorgames.com>
2020-07-23 14:19:19 +0800

    优化EventCenter

--- assets/Script/UIFrame/EventCenter.ts
+++ assets/Script/UIFrame/EventCenter.ts
@@ -3,16 +3,16 @@ import { Pool, IPool } from "./common/Pool";
 
 let idSeed = 1;         // 这里有一个小缺陷就是idSeed有最大值,Number.MAX_VALUE
 export class EventCenter {
-    private _listeners: {[eventName: string]: {[id: string]: Array<EventInfo>}} = cc.js.createMap();
-    private _dispatching : number = 0;
-    private _removeCommands : RemoveCommand[] = [];
-    private uuid : string = "0";
+    private static _listeners: {[eventName: string]: {[id: string]: Array<EventInfo>}} = cc.js.createMap();
+    private static _dispatching : number = 0;
+    private static _removeCommands : RemoveCommand[] = [];
+    private static uuid : string = "0";
 
-    private _eventPool: Pool<EventInfo> = new Pool<EventInfo>(() => {
+    private static _eventPool: Pool<EventInfo> = new Pool<EventInfo>(() => {
         return new EventInfo();
     }, 10);
 
-    public on(eventName: string, callback: Function, target: any = undefined, once = false) {
+    public static on(eventName: string, callback: Function, target: any = undefined, once = false) {
         target = target || this;
         let targetId = target['uuid'] || target['id'];
         if(targetId === undefined) {
@@ -20,10 +20,10 @@ export class EventCenter {
         }
         this.onById(eventName, targetId, target, callback, once);
     }
-    public once(eventName: string, callback: Function, target: any = undefined) {
+    public static once(eventName: string, callback: Function, target: any = undefined) {
         this.on(eventName, callback, target, true);
     }
-    private onById(eventName: string, targetId: string, target: any, cb: Function, once: boolean) {
+    private static onById(eventName: string, targetId: string, target: any, cb: Function, once: boolean) {
         let collection = this._listeners[eventName];
         if(!collection) {
             collection = this._listeners[eventName] = {};
@@ -36,13 +36,13 @@ export class EventCenter {
         eventInfo.init(cb, target, once);
         events.push(eventInfo);
     }
-    public off(eventName: string, callback: Function, target: any = undefined) {
+    public static off(eventName: string, callback: Function, target: any = undefined) {
         target = target || this;
         let targetId = target['uuid'] || target['id'];
         if(!targetId) return ;
         this.offById(eventName, callback, targetId);
     }
-    public targetOff(target: any) {
+    public static targetOff(target: any) {
         target = target || this;
         let targetId = target['uuid'] || target['id'];
         if(!targetId) return ;
@@ -53,15 +53,15 @@ export class EventCenter {
             }
         }
     }
-    private offById(eventName: string, callback: Function, targetId: string) {
+    private static offById(eventName: string, callback: Function, targetId: string) {
         if(this._dispatching > 0) {
             let cmd = new RemoveCommand(eventName, callback, targetId);
             this._removeCommands.push(cmd);
         }else {
-            this.odOff(eventName, callback, targetId);
+            this.doOff(eventName, callback, targetId);
         }
     }
-    private odOff(eventName: string, callback: Function, targetId: string) {
+    private static doOff(eventName: string, callback: Function, targetId: string) {
         let collection = this._listeners[eventName];
         if(!collection) return ;
         let events = collection[targetId];
@@ -76,17 +76,17 @@ export class EventCenter {
         }
     }
 
-    private doRemoveCommands() {
+    private static doRemoveCommands() {
         if(this._dispatching !== 0) {
             return;
         }
         for(let cmd of this._removeCommands) {
-            this.odOff(cmd.eventName, cmd.callback, cmd.targetId);
+            this.doOff(cmd.eventName, cmd.callback, cmd.targetId);
         }
         this._removeCommands.length = 0;
     }
     
-    public emit(eventId: string, ...param: any[]) {
+    public static emit(eventId: string, ...param: any[]) {
         let collection = this._listeners[eventId];
         if(!collection) return false;
         this._dispatching ++;
