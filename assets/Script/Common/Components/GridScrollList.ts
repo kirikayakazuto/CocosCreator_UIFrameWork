0d736e2ffc4547bffd0095cce94e9d02433c6b2d
denglang <denglang@hortorgames.com>
2020-07-24 11:52:48 +0800

    优化taskMgr

---
+++ assets/Script/Common/Components/GridScrollList.ts
@@ -0,0 +1,83 @@
+import { ScrollViewHelper, ScrollViewElementProxy } from "../../Common/Components/ScrollViewHelper";
+
+/*
+ * @Author: liuhandong
+ * @Date: 2020-01-02 17:46:49
+ */
+const {ccclass, property} = cc._decorator;
+
+enum RowDirection {
+    LeftToRight,
+    TopToBottom
+}
+
+@ccclass
+export class GridScrollList extends cc.Component {
+    public allProxy : ScrollViewElementProxy[] = [];
+    @property(ScrollViewHelper)
+    public scrollHelper:ScrollViewHelper = null;
+    @property({type:cc.Enum(RowDirection)})
+    private rowDirection:RowDirection = RowDirection.LeftToRight;
+    @property()
+    private colSpace : number = 10;
+    @property()
+    private rowSpace : number = 10;
+    @property()
+    private colCount : number = 3;
+    @property()
+    private margin : number = 10;
+
+    public doLayout() {
+        this._layoutItems();
+    }
+
+    private _layoutItems() {
+        this.scrollHelper.clearData();
+        let data = this.allProxy;
+        let elemSize = data.length?data[0].region.size:cc.size(0, 0);
+        let xSpace = this.colSpace;
+        let ySpace = this.rowSpace;
+        let colCount = this.colCount;
+        let viewSize = this.scrollHelper.scrollView.node.getContentSize();
+        if(this.rowDirection == RowDirection.TopToBottom) {
+            let temp = viewSize.width;
+            viewSize.width = viewSize.height;
+            viewSize.height = temp;
+            temp = elemSize.width;
+            elemSize.width = elemSize.height;
+            elemSize.height = temp;
+        }
+        let yMargin = this.margin;
+        let xMargin = (viewSize.width - (colCount * elemSize.width + (colCount - 1) * xSpace)) / 2;
+        
+
+        let x = xMargin;
+        let y = - yMargin + ySpace + elemSize.height;
+        let dy = -ySpace - elemSize.height;
+        let dx = xSpace + elemSize.width;
+        if(this.rowDirection == RowDirection.TopToBottom) {
+            dy = ySpace + elemSize.height;
+            dx = -xSpace - elemSize.width;
+            y = yMargin - dy;
+        }
+        for(let i = 0; i < data.length; i++) {
+            x += dx;
+            if(i % colCount == 0) {
+                y += dy;
+                x = xMargin;
+            }
+            let proxy = data[i];
+            if(this.rowDirection == RowDirection.LeftToRight) {
+                proxy.region.origin = cc.v2(x, y - elemSize.height);
+            } else {
+                proxy.region.origin = cc.v2(y, x - elemSize.height);
+            }
+            this.scrollHelper.addData(proxy);
+        }
+        if(this.rowDirection == RowDirection.LeftToRight) {
+            this.scrollHelper.scrollView.content.setContentSize(viewSize.width, -y + elemSize.height + yMargin);
+        } else {
+            this.scrollHelper.scrollView.content.setContentSize(y + elemSize.height + yMargin, viewSize.width);
+        }
+    }
+}
---
+++ assets/Script/UIFrame/TaskMgr.ts
@@ -0,0 +1,55 @@
+import PriorityQueue from "../Common/Utils/PriorityQueue";
+import { pid } from "process";
+
+/**
+ * author: denglang
+ * desc: 一个命令队列， 
+ */
+export enum CommandType {
+    // 自定义命令类型
+}
+export class Command {
+    type: CommandType;
+    data: any;
+}
+
+/** 命令队列 */
+export default class TaskMgr {
+
+    private static _instance: TaskMgr = null;
+    public static get inst() {
+        if(!this._instance) {
+            this._instance = new TaskMgr();
+        }
+        return this._instance;
+    }
+
+    private _cmdMap: {[key:string]: PriorityQueue<Command>} = cc.js.createMap(); 
+
+    private _debug = false;
+    public setDebug(debug = true) {
+        this._debug = debug;
+    }
+
+    /**
+     * @param command 命令
+     * @param pIdx 优先级 数值越大优先级越高  默认是0
+     */
+    public pushCommand(key: string, command: Command, pIdx: number = 0) {
+        let cmdQueue = this._cmdMap[key];
+        if(!cmdQueue) {
+            cmdQueue = this._cmdMap[key] = new PriorityQueue<Command>();
+        }
+        cmdQueue.enqueue(command, pIdx);
+    }
+
+    public popCommand(key: string) {
+        let cmdQueue = this._cmdMap[key];
+        if(!cmdQueue) return null;
+        return cmdQueue.dequeue();
+    }
+
+    public hasCommand() {
+
+    }
+}
