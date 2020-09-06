02c2c0ab0bdfe6e132fa4eba69ef1a27bbc035bf
denglang <denglang@hortorgames.com>
2020-09-06 23:13:31 +0800

    feat: 为modal添加默认值

--- assets/Script/UIFrame/FrameType.ts
+++ assets/Script/UIFrame/FrameType.ts
@@ -1,12 +1,12 @@
 import { MaskOpacity } from "./config/SysDefine";
 
 export class MaskType {
-    public opacity: MaskOpacity = MaskOpacity.None;
+    public opacity: MaskOpacity = MaskOpacity.OpacityHalf;
     public clickMaskClose = false;      // 点击阴影关闭
     public isEasing = true;             // 缓动实现
     public easingTime = 0.2;            // 缓动时间
 
-    constructor(opacity = MaskOpacity.None, ClickMaskClose=false, IsEasing = true, EasingTime=0.2) {
+    constructor(opacity = MaskOpacity.OpacityHalf, ClickMaskClose=false, IsEasing = true, EasingTime=0.2) {
         this.opacity = opacity;
         this.clickMaskClose = ClickMaskClose;
         this.isEasing = IsEasing;
