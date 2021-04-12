a134222fd69e57b9f2c819ddcb85b25f7dc7888f
joudeng <joudeng@tencent.com>
2021-04-12 15:42:20 +0800

    feat: 优化

--- assets/Script/Common/Components/BatchComponent.ts
+++ assets/Script/Common/Components/BatchComponent.ts
@@ -1,9 +1,19 @@
 import BatchAssembler from "../Assemblers/BatchAssembler";
 
+enum TraversalMode {
+    Default,
+    SameName,
+
+}
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class BantchComponent extends cc.RenderComponent {
+
+    @property({type: cc.Enum(TraversalMode), tooltip: "遍历模式: Default 默认的广度遍历, SameName 同名结点同批次"})
+    mode: TraversalMode = 0;
+
     onEnable() {
         super.onEnable();
         if (!CC_NATIVERENDERER) this.node._renderFlag |= cc.RenderFlow.FLAG_POST_RENDER;    
