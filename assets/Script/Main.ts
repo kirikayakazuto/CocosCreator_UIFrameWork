52dafe9df47e5bec5c71363a30609ea832fa644d
denglang <1099263878@qq.com>
2021-04-17 23:15:17 +0800

    12312

--- assets/Script/Main.ts
+++ assets/Script/Main.ts
@@ -29,21 +29,6 @@ export default class Main extends cc.Component {
             DebugWindowUtil.init();
         }
 
-        // TipsMgr.inst.setLoadingForm("UIForms/UILoading");
-        UILogin.openView(1, 2, 3);
-        // UITest.openView();
-        // UICapture.openView();
-        // UIDungeon.openView();
-        this.buttonPlus.addClick(() => {
-            cc.log("点击事件!");
-        }, this);
-
-        this.buttonPlus.addLongClick(() => {
-            cc.log("触发长按事件 开始");
-        }, () => { 
-            cc.log("触发长按事件 结束");
-        }, this);
-
         this.touchPlus.addEvent((e) => {
             console.log('触发点击事件');
         }, (e) => {
