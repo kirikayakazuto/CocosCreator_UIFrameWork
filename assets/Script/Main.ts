55c9e81b9a15b54ec054736d91b4dfb11eb84da7
denglang <1099263878@qq.com>
2021-04-17 23:01:42 +0800

    feat: 优化

--- assets/Script/Main.ts
+++ assets/Script/Main.ts
@@ -1,43 +1,20 @@
-import UILogin from "./test/UILogin";
-import { EventCenter } from "./UIFrame/EventCenter";
-import DebugWindowUtil from "./Common/Utils/DebugWindowUtils";
-const DIRTY_PROP = cc.RenderFlow.FLAG_OPACITY | cc.RenderFlow.FLAG_WORLD_TRANSFORM;
+import UILoading from "./test/UILoading";
 
 const {ccclass, property} = cc._decorator;
 
-/** 禁止子节点执行的FLAG */
-const BAN_FALG = cc.RenderFlow.FLAG_RENDER | cc.RenderFlow.FLAG_POST_RENDER;
-
 @ccclass
 export default class Main extends cc.Component {
-
-    @property(cc.Node)
-    ndRoot: cc.Node = null;
-
-    @property(cc.Prefab)
-    pfItem: cc.Prefab = null;
     
     onLoad() {
         cc.dynamicAtlasManager.enabled = true;
     }
 
     start () {
-        // TipsMgr.inst.setLoadingForm("UIForms/UILoading");
-        // UILogin.openView(1);
-        // UITest.openView();
-        // UICapture.openView();
-        // UIDungeon.openView();
-        this.test();
+        UILoading.openView();
     }
 
-    test() {
-    }
-
-    /**
-     * 
-     */
     onDestroy() {
-        cc.log('destory');
+
     } 
     
 }
