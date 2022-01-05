e62a40ae2b158db8b8218ab09a83b57b3e1783f6
denglang <denglang@bytedance.com>
2022-01-05 16:48:33 +0800

    feat: 删除了一些没啥用的代码

--- assets/Script/ECS/TreeConfig.ts
+++
@@ -1,6 +0,0 @@
-export default class TreeConfig {
-    static tree = [
-        "Canvas",
-        
-    ];
-}
--- assets/Script/Main.ts
+++ assets/Script/Main.ts
@@ -1,9 +1,4 @@
-import { Batcher } from "./ECS/Graphics/Batcher";
-import { Input } from "./ECS/Input/Input";
-import { KeyboardUtils } from "./ECS/Input/KeyboardUtils";
-import BaseScene from "./ECS/Scenes/BaseScene";
 import SceneMgr from "./UIFrame/SceneMgr";
-import WindowMgr from "./UIFrame/WindowMgr";
 import UIConfig from "./UIScript/UIConfig";
 
 const {ccclass, property} = cc._decorator;
@@ -16,22 +11,10 @@ export default class Main extends cc.Component {
 
     start () {
         SceneMgr.open(UIConfig.Navigator.prefabUrl);
-        // es.Core.debugRenderEndabled = true;
-        // es.Core.create(true);
 
-        // es.Graphics.instance = new es.Graphics(new Batcher());
-
-        // KeyboardUtils.init();
-        // Input.initialize();
-
-        // es.Core.scene = new BaseScene();
     }
 
     onDestroy() {
 
-    }  
-    
-    update(dt: number) {
-        // es.Core.emitter.emit(es.CoreEvents.frameUpdated, dt);
-    }
+    }      
 }
